3651ec71fbb1e25383f9fbfb1561478e5fbbbd59
Omar-Azam <thisisomarazam@gmail.com>
2024-12-25 22:43:07 +0500

    UI/UX Added

--- js/module4.js
+++ js/module4.js
@@ -1,13 +1,174 @@
 let nextQues = [
-    {
-        question: "Q.2. ",
-        option1: "",
-        option2: "",
-        option3: "",
-        option4: "",
-    },
+  {
+    question: "Q.2 ____________ don't work in Internet Explorer 8 and below.",
+    option1: "Adaptable Grids",
+    option2: "Viewpoints",
+    option3: "Spreadsheets",
+    option4: "Media queries",
+  },
+  {
+    question:
+      "Q.3 Which of the following devices is not supported by Responsive Web Design?",
+    option1: "Smartphone",
+    option2: "Notebook",
+    option3: "Tablet",
+    option4: "Camera",
+  },
+  {
+    question:
+      "Q.4 Which of the following technologies are used by Responsive Web Design to resize, hide, shrink, enlarge, and move the content on any screen?",
+    option1: "CSS",
+    option2: "Java",
+    option3: "CSS and HTML",
+    option4: "XML",
+  },
+  {
+    question:
+      "Q.5 ________ is used to arrange the grid columns in proportion based on page element sizing.",
+    option1: "Fluid Grid",
+    option2: "Grid",
+    option3: "Liquid Grid",
+    option4: "Fluid",
+  },
+  {
+    question:
+      "Q.6 Content audit should always be recorded in a _____________, mainly because they are flexible.",
+    option1: "Hierarchy",
+    option2: "Spreadsheet",
+    option3: "Tags",
+    option4: "HTML",
+  },
+  {
+    question:
+      "Q.7 ____________ concentrates on setting up an engaging interface with logic behaviors.",
+    option1: "Interaction Design",
+    option2: "Information Architecture",
+    option3: "Visual Design",
+    option4: "Dropdown List",
+  },
+  {
+    question:
+      "Q.8 __________ is a navigation aid that allows the user to keep track of the location within programs.",
+    option1: "Pagination",
+    option2: "Tag",
+    option3: "Breadcrumb",
+    option4: "Search Field",
+  },
+  {
+    question:
+      "Q.9 Which of the following principles emphasizes the importance of designing the user interface to prevent users from making errors.",
+    option1: "Feedback",
+    option2: "Visibility",
+    option3: "Simplicity",
+    option4: "Tolerance",
+  },
+  {
+    question:
+      "Q.10 ______________ allows the user to speak in the normal everyday language in order to interact with the system.",
+    option1: "Natural Language Interface",
+    option2: "Command Language Based Interface",
+    option3: "Web Based Interface",
+    option4: "Menu Based Interface",
+  },
+  {
+    question:
+      "Q.11 Which phase of the Responsive Work Design workflow consists of the processes called UX sketches and Interaction design?",
+    option1: "Discover",
+    option2: "Design",
+    option3: "Develop",
+    option4: "Deploy",
+  },
+  {
+    question:
+      "Q.12 The footprint of the CSS file on slower devices can be reduced by smart positioning of media queries and ____________.",
+    option1: "Progressive Enhancement",
+    option2: "Size Optimization",
+    option3: "Low Load Time",
+    option4: "Breakpoints",
+  },
+  {
+    question:
+      "Q.13 __________ are browser widths that have a media question presentation to change the format.",
+    option1: "Spreadsheets",
+    option2: "Breakpoints",
+    option3: "Stackings",
+    option4: "Tab Bars",
+  },
+  {
+    question:
+      "Q.14 _______ is the action where all the content on a Website is checked and compiled into a big list.",
+    option1: "Scripting",
+    option2: "Content Audit",
+    option3: "Deployment",
+    option4: "Designing",
+  },
+  {
+    question:
+      "Q.15 What allows swift navigation between different parts of an application?",
+    option1: "Bottom Bar",
+    option2: "Slide Down Menu",
+    option3: "Navigation Drawer",
+    option4: "Black Box Technique",
+  },
+  {
+    question:
+      "Q.16 Which phase of the Responsive Work Design workflow consists of the processes called content migration and user acceptance testing?",
+    option1: "Deploy",
+    option2: "Develop",
+    option3: "Discover",
+    option4: "Design",
+  },
+  {
+    question: "Q.17 An adaptable matrix can be made using _______________.",
+    option1: "CSS3 Module",
+    option2: "XML",
+    option3: "HTML5",
+    option4: "CSS2 Syntax",
+  },
+  {
+    question: "Q.18 ______________ verifies the loading time of the Website.",
+    option1: "Accessibility",
+    option2: "Navigation",
+    option3: "Content",
+    option4: "Usability",
+  },
+  {
+    question:
+      "Q.19 Which layer of the Progressive Enhancement uses JavaScript for improved usability?",
+    option1: "First Layer",
+    option2: "Second Layer",
+    option3: "Third Layer",
+    option4: "Fourth Layer",
+  },
+  {
+    question:
+      "Q.20 Which of the following is not the normal screen width for designers?",
+    option1: "320",
+    option2: "420",
+    option3: "760",
+    option4: "960",
+  },
 ];
 
 let crrAns = [
-    "",
+  "4", // Q.1. Digital Marketing
+  "4", // Q.2. Media queries
+  "4", // Q.3. Camera
+  "3", // Q.4. CSS and HTML
+  "1", // Q.5. Fluid Grid
+  "2", // Q.6. Spreadsheet
+  "1", // Q.7. Interaction Design
+  "3", // Q.8. Breadcrumb
+  "4", // Q.9. Tolerance
+  "1", // Q.10. Natural Language Interface
+  "2", // Q.11. Design
+  "1", // Q.12. Progressive Enhancement
+  "2", // Q.13. Breakpoints
+  "2", // Q.14. Content Audit
+  "3", // Q.15. Navigation Drawer
+  "1", // Q.16. Deploy
+  "1", // Q.17. CSS3 Module
+  "4", // Q.18. Usability
+  "3", // Q.19. Third Layer
+  "2", // Q.20. 420
 ];
